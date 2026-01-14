61e1edf08171a452f12beae52849d3336d68d17d
Seth <sdarmistead@gmail.com>
2026-01-14 11:45:08 +0100

    got parallax library

---
+++ index.js
@@ -0,0 +1,6 @@
+import SimpleParallax from "simple-parallax-js/vanilla";
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
+
+//You can also choose to apply the parallax on multiple images:
