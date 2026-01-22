cb6bc5915bec886fc0d86658c123b8612be702f0
Seth Armistead <sdarmistead@gmail.com>
2026-01-22 22:33:24 +0100

    added a lot. Fixed animations, did media queries, added content, screenshots, background, and more

--- general.js
+++ general.js
@@ -1,4 +1,7 @@
+AOS.init()
+
 const HAMBURGER= document.querySelector('.hamburger')
+const NAV = document.querySelector('.nav')
 
 HAMBURGER.addEventListener('click', ()=>{
   document.querySelector('.nav').classList.toggle('nav-toggle')
